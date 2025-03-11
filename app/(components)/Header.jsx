d15ec60d500979a0fd862eb3ea01a494e1d47093
AleksandrKompanetc <moneymacker7@gmail.com>
2025-03-11 19:02:41 +0200

    adding component Hero

--- app/(components)/Header.jsx
+++ app/(components)/Header.jsx
@@ -1,6 +1,6 @@
 const Header = () => {
   return (
-    <header className="fixed top-0 left-0 right-0 bg-white shadow-md z-50">
+    <header className="fixed top-0 left-0 right-0 bg-gray shadow-md z-50">
       Header
     </header>
   );
