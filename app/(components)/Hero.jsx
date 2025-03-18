351bc676cd2b0e4b45d902dc8b52ed12dbe31a75
AleksandrKompanetc <moneymacker7@gmail.com>
2025-03-18 22:48:37 +0200

    update Hero component to enhance wardrobe promotion with new heading and product section

--- app/(components)/Hero.jsx
+++ app/(components)/Hero.jsx
@@ -4,7 +4,13 @@ const Hero = () => {
   return (
     <section className='max-padd-container'>
       <div>
-        <div>Elevate your</div>
+        <div>
+          <h2>Elevate your wardrobe with trendy fashion from <span>Shop</span></h2>
+          <div>
+            <span>Latest Product</span>
+            <div></div>
+          </div>
+        </div>
       </div>
     </section>
   )
