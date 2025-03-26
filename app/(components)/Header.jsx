35c7ea6ee4bef73140f1454347cc7330d6e3fa57
AleksandrKompanetc <moneymacker7@gmail.com>
2025-03-26 23:42:41 +0200

    refactor Header component to use semantic HTML elements and improve structure

--- app/(components)/Header.jsx
+++ app/(components)/Header.jsx
@@ -4,23 +4,25 @@ import { Search } from 'lucide-react';
 
 const Header = () => {
   return (
-    <div className='flex justify-between items-center width-full max-padd-container px-10 py-4'>
-      {/* LOGO */}
-      <Link href={'/'} className='flex flex-col justify-between gap-2'>
-        <span className='relative top-2'>MegaShop</span>
-        <span>Marketplace</span>
-      </Link>
+    <header className='flex justify-between items-center width-full max-padd-container px-10 py-4'>
+      <div>
+        {/* LOGO */}
+        <Link href={'/'} className='flex flex-col justify-between gap-2'>
+          <span className='relative top-2'>MegaShop</span>
+          <span>Marketplace</span>
+        </Link>
 
-      <div className='hidden md:flex rounded-full bg-white w-[444px] pl-6 gray'>
-        <input type="text" placeholder='Type here...' className='bg-transparent w-full outline-none text-14px' />
-        <Search className='cursor-pointer rounded-full h-10 w-11 p-2.5 m-1' />
-      </div>
+        <nav className='hidden md:flex rounded-full bg-white w-[444px] pl-6 gray'>
+          <input type="text" placeholder='Type here...' className='bg-transparent w-full outline-none text-14px' />
+          <Search className='cursor-pointer rounded-full h-10 w-11 p-2.5 m-1' />
+        </nav>
 
-      <div className='flex gap-15'>
-        <button className='border-none transparent'>Login</button>
-        <button className='shadow-none'>Sign Up</button>
+        <div className='flex gap-15'>
+          <button className='border-none transparent'>Login</button>
+          <button className='shadow-none'>Sign Up</button>
+        </div>
       </div>
-    </div>
+    </header>
   );
 };
 
