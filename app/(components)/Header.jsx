9d4408394a579794f8f20420d72fdf6e5e303cb8
AleksandrKompanetc <moneymacker7@gmail.com>
2025-03-29 23:31:30 +0200

    refactor Header component to enhance layout with improved spacing and alignment

--- app/(components)/Header.jsx
+++ app/(components)/Header.jsx
@@ -6,7 +6,7 @@ import { Menu } from 'lucide-react';
 const Header = () => {
   return (
     <header className='sticky top-0 z-50 bg-white shadow-md'>
-      <div className='container mx-auto flex h-16'>
+      <div className='container mx-auto flex h-16 items-center justify-between px-4 md:px-6'>
         {/* LOGO */}
         <Link href={'/'} className='text-xl font-bold text-gray-900'>
           <span className='relative top-2'>MegaShop</span>
