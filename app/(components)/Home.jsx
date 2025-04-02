6d0e548327e74c4fb463c98880557490fce76133
AleksandrKompanetc <moneymacker7@gmail.com>
2025-04-02 23:57:08 +0300

    update Home component to display product images and structure, enhancing visual appeal and interactivity

--- app/(components)/Home.jsx
+++ app/(components)/Home.jsx
@@ -19,8 +19,11 @@ const Home = () => {
       <h1 className='text-2xl font-bold mb-4'>MarketPlace</h1>
       <div className='grid grid-cols-3 gap-4'>
         {products.map((product) => (
-          <div>
-            
+          <div key={product.id}>
+            <img src={product.image} alt={product.name} className='border p-4 rounded-lg shadow' />
+            <h2></h2>
+            <p></p>
+            <button></button>
           </div>
         ))}
       </div>
