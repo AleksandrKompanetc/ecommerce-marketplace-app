4ecf602b8dd656c584e1fd304845db49291cdfd7
AleksandrKompanetc <moneymacker7@gmail.com>
2025-04-08 23:52:58 +0300

    remove layout, Provider, Header, Home, ProductCard components and associated mock data for project restructuring

--- app/layout.tsx
+++ app/layout.tsx
@@ -24,17 +24,6 @@ interface RootLayoutProps {
 
 export default function RootLayout({ children }: RootLayoutProps) {
   return (
-    <html lang="en" suppressHydrationWarning>
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased bg-gray-50`}
-      >
-        <Provider>
-          <Header />
-          <main className="pt-16 container mx-auto px-4">
-            {children}
-          </main>
-        </Provider>
-      </body>
-    </html>
+    
   );
 } 
