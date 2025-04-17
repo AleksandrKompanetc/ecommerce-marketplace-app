42fcf7f262e1460bc591521157be2624d0fcb72e
AleksandrKompanetc <moneymacker7@gmail.com>
2025-04-17 23:59:33 +0300

    changed Home

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,8 @@
 import Image from "next/image";
+import { stripe } from "@/lib/stripe";
 
-export default function Home() {
+export default async function Home() {
+  const products = await stripe.products.list()
   return (
     <div className="text-blue-500">
      
