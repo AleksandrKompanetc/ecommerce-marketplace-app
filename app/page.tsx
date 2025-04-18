cbdee43400cb056cfc7b458e1a9b66d47c5b1f23
AleksandrKompanetc <moneymacker7@gmail.com>
2025-04-18 23:43:03 +0300

    added some changes for Home page

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,12 @@ import Image from "next/image";
 import { stripe } from "@/lib/stripe";
 
 export default async function Home() {
-  const products = await stripe.products.list()
+  const products = await stripe.products.list({
+    expand: ["data.default_price"],
+    limit: 5
+  })
+
+  console.log(products)
   return (
     <div className="text-blue-500">
      
