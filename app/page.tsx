285f0dac93b82514e13db77b225220ae25b56949
AleksandrKompanetc <moneymacker7@gmail.com>
2025-04-19 23:34:13 +0300

    creating component Carousel

--- app/page.tsx
+++ app/page.tsx
@@ -20,9 +20,10 @@ export default async function Home() {
             <Link href="/products">Browse All Products</Link>
           </Button>
         </div>
-        <Image alt="Banner Image" width={450} height={450}></Image>
+        <Image alt="Banner Image" width={450} height={450} src={products.data[0].images[0]} />
       </div>
      </section>
+     
     </div>
   );
 }
---
+++ components/carousel.tsx
@@ -0,0 +1,3 @@
+export default Carousel = () => {
+  return <div></div>
+}
