3a25c8007fc58a8dc8f2946f4b6107e8b789ad0d
AleksandrKompanetc <moneymacker7@gmail.com>
2025-04-21 23:45:36 +0300

    adding Card

--- components/carousel.tsx
+++ components/carousel.tsx
@@ -1,9 +1,10 @@
 import Stripe from "stripe"
+import { Card } from "./ui/card"
 
 interface Props {
   products: Stripe.Product
 }
 
 export const Carousel = ({products}: Props) => {
-  return <div></div>
+  return <Card></Card>
 }
