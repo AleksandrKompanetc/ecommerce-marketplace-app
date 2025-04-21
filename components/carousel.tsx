a59ba000cb58f6b65dcb54875e446bc1e0b8aa47
AleksandrKompanetc <moneymacker7@gmail.com>
2025-04-21 23:57:38 +0300

    added changes for Carousel

--- components/carousel.tsx
+++ components/carousel.tsx
@@ -1,13 +1,19 @@
 import Stripe from "stripe"
 import { Card } from "./ui/card"
-import { useState } from "react"
+import { useEffect, useState } from "react"
 
 interface Props {
-  products: Stripe.Product
+  products: Stripe.Product[]
 }
 
 export const Carousel = ({products}: Props) => {
   const [current, setCurrent] = useState<number>(0)
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCurrent((prev) => prev + 1)
+    }, 3000)
+  }, [products.length])
 
   return <Card></Card>
 }
