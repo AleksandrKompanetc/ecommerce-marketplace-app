441bd71657c30affd04e805b87e7d3dee8df8f03
AleksandrKompanetc <moneymacker7@gmail.com>
2025-04-21 23:47:30 +0300

    adding state

--- components/carousel.tsx
+++ components/carousel.tsx
@@ -1,10 +1,13 @@
 import Stripe from "stripe"
 import { Card } from "./ui/card"
+import { useState } from "react"
 
 interface Props {
   products: Stripe.Product
 }
 
 export const Carousel = ({products}: Props) => {
+  const [current, setCurrent] = useState<number>(0)
+
   return <Card></Card>
 }
