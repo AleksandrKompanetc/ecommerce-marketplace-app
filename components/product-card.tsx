9d8cb878e111c81e728ccb4783cbc19802785cce
AleksandrKompanetc <moneymacker7@gmail.com>
2025-04-26 13:21:38 +0300

    changed Card

--- components/product-card.tsx
+++ components/product-card.tsx
@@ -1,6 +1,7 @@
 import Link from "next/link"
 import Stripe from "stripe"
 import { Card } from "./ui/card"
+import Image from "next/image"
 
 interface Props {
   product: Stripe.Product
@@ -8,6 +9,18 @@ interface Props {
 
 export const ProductCard = ({ product }: Props) => {
   return <Link href={"/products/1"}>
-    <Card></Card>
+    <Card>
+    {product.images && product.images[0] && (
+        <div className="relative h-80 w-full">
+          <Image
+            alt={product.name}
+            src={product.images[0]}
+            layout="fill"
+            objectFit="cover"
+            className="transition-opacity duration-500 ease-in-out"
+          />
+        </div>
+      )}
+    </Card>
   </Link>
 }
