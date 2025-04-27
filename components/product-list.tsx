f2635c91d6ff81e0c2d28894dc310e5532df5381
AleksandrKompanetc <moneymacker7@gmail.com>
2025-04-27 23:59:08 +0300

    added changes

--- components/product-list.tsx
+++ components/product-list.tsx
@@ -1,11 +1,15 @@
 import Stripe from "stripe"
 import { ProductCard } from "./product-card"
+import { useState } from "react"
 
 interface Props {
   products: Stripe.Product[]
 }
 
 export const ProductList = ({ products }: Props) => {
+
+  const [searchTerm, setSearch] = useState<string>("")
+  
   return (
     <div>
       <div>
