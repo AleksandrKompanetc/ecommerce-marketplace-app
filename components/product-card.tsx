6fc0bd903fe78f30a1d29d587c5f4ef61813dc9a
AleksandrKompanetc <moneymacker7@gmail.com>
2025-04-29 22:42:52 +0300

    added changes

--- components/product-card.tsx
+++ components/product-card.tsx
@@ -11,7 +11,7 @@ interface Props {
 export const ProductCard = ({ product }: Props) => {
   const price = product.default_price as Stripe.Price
   return (
-    <Link href={"/products/1"} className="block h-full">
+    <Link href={`/products/${product.id}`} className="block h-full">
       <Card className="group hover:shadow-2xl transition duration-300 py-0 h-full flex flex-col border-none">
         {product.images && product.images[0] && (
           <div className="relative h-60 w-full">
