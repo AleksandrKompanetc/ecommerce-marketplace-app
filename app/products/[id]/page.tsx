bae1b30c1a3898b667860987f9e23f45336097a3
AleksandrKompanetc <moneymacker7@gmail.com>
2025-04-29 23:10:15 +0300

    changed page

---
+++ app/products/[id]/page.tsx
@@ -0,0 +1,8 @@
+import { stripe } from "@/lib/stripe"
+
+export default async function ProductPage({ params }: { params: { id: string } }) {
+  const product = stripe.products.retrieve(paramid, {
+    expand: ["default_price"],
+  })
+  return <ProductDetail />
+}
