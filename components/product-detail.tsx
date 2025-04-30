482be428bacfd73e809967388458d2798c41b3eb
AleksandrKompanetc <moneymacker7@gmail.com>
2025-04-30 13:38:25 +0300

    adding some styles

--- components/product-detail.tsx
+++ components/product-detail.tsx
@@ -22,9 +22,10 @@ export const ProductDetail = ({product}: Props) => {
           />
         </div>
       )}
-      <div>
-        <h1>{product.name}</h1>
-        {product.description && <p>{product.description}</p>}
+      <div className="md:w-1/2">
+        <h1 className="text-3xl font-bold mb-4">{product.name}</h1>
+        {product.description && 
+        <p className="text-gray-700 mb-4">{product.description}</p>}
 
         {price && price.unit_amount && (
           <p className="text-lg font-semibold text-gray-900">
@@ -32,7 +33,7 @@ export const ProductDetail = ({product}: Props) => {
           </p>
         )}
 
-        <div>
+        <div className="flex items-center space-x-4">
           <Button variant="outline"> -</Button>
           <span> 0</span>
           <Button variant="outline"> +</Button>
