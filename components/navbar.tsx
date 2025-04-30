67938733a21fb740c9c33d29ad6055d26c860683
AleksandrKompanetc <moneymacker7@gmail.com>
2025-04-30 23:04:39 +0300

    changing styles for basket in navbar

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -3,7 +3,7 @@
 import Link from "next/link"
 import { ShoppingCartIcon, Bars3Icon, XMarkIcon } from "@heroicons/react/24/outline"
 import { useCartStore } from "@/store/cart-store"
-import { useState } from "react"
+import { useState, useEffect } from "react"
 import { Button } from "./ui/button"
 
 export const Navbar = () => {
@@ -36,12 +36,23 @@ export const Navbar = () => {
           </Link>
         </div>
         <div className="flex items-center space-x-4">
-          <Link href="/checkout">
-            <ShoppingCartIcon />
-            {cartCount > 0 && <span>{cartCount}</span>}
+          <Link href="/checkout" className="relative">
+            <ShoppingCartIcon className="h-6 w-6" />
+            {cartCount > 0 &&
+              <span className="absolute -top-2 -right-2 flex h-5 w-5 items-center justify-center text-xs text-white rounded-lg bg-red-600">
+                {cartCount}
+              </span>
+            }
           </Link>
-          <Button variant="ghost" onClick={() => setMobileOpen((prev) => !prev)}>
-            {mobileOpen ? <XMarkIcon /> : <Bars3Icon />}
+          <Button
+            variant="ghost"
+            className="md:hidden"
+            onClick={() => setMobileOpen((prev) => !prev)}>
+            {mobileOpen ? (
+              <XMarkIcon className="h-6 w-6" />
+            ) : (
+              <Bars3Icon className="h-6 w-6" />
+            )}
           </Button>
         </div>
       </div>
@@ -50,6 +61,12 @@ export const Navbar = () => {
           <ul>
             <li>
               <Link href={"/"}>Home</Link>
+            </li>
+            <li>
+              <Link href={"/products"}>Products</Link>
+            </li>
+            <li>
+              <Link href={"/checkout"}>Checkout</Link>
             </li>
           </ul>
         </nav>
