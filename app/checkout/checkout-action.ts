591a9de2ecc50a8cf0609f174629e40ecb51275f
AleksandrKompanetc <moneymacker7@gmail.com>
2025-05-01 23:42:33 +0300

    added some changes for checkoutAction

--- app/checkout/checkout-action.ts
+++ app/checkout/checkout-action.ts
@@ -1,1 +1,20 @@
-export const checkoutAction = async (formData: FormData): Promise<void> => {}
+import { stripe } from "@/lib/stripe"
+import { CartItem } from "@/store/cart-store"
+
+export const checkoutAction = async (formData: FormData): Promise<void> => {
+
+  const itemsJson = formData.get("items") as string
+  const items = JSON.parse(itemsJson)
+  const line_items = items.map((item: CartItem) => ({
+    price_data: {
+      currency: "cad",
+      product_data: {name: item.name},
+      unit_amount: item.price
+    },
+    quantity: item.quantity,
+  }))
+
+  const session = await stripe.checkout.sessions.create({
+    
+  })
+}
