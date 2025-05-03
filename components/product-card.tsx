fed4fc534d6062e3681e4f4612917173a879bb54
AleksandrKompanetc <moneymacker7@gmail.com>
2025-05-03 23:54:05 +0300

    fixed component Product Card

--- components/product-card.tsx
+++ components/product-card.tsx
@@ -1,18 +1,19 @@
-import Link from "next/link"
-import Stripe from "stripe"
-import { Card, CardContent, CardHeader, CardTitle } from "./ui/card"
-import Image from "next/image"
-import { Button } from "./ui/button"
+import Link from "next/link";
+import Stripe from "stripe";
+import { Card, CardContent, CardHeader, CardTitle } from "./ui/card";
+import Image from "next/image";
+import { Button } from "./ui/button";
 
 interface Props {
-  product: Stripe.Product
+  product: Stripe.Product;
 }
 
 export const ProductCard = ({ product }: Props) => {
-  const price = product.default_price as Stripe.Price
+  const price = product.default_price as Stripe.Price;
+
   return (
-    <Link href={`/products/${product.id}`} className="block h-full">
-      <Card className="group hover:shadow-2xl transition duration-300 py-0 h-full flex flex-col border-none">
+    <Card className="group hover:shadow-2xl transition duration-300 py-0 h-full flex flex-col border-none">
+      <Link href={`/products/${product.id}`} className="block h-full">
         {product.images && product.images[0] && (
           <div className="relative h-60 w-full">
             <Image
@@ -24,21 +25,27 @@ export const ProductCard = ({ product }: Props) => {
             />
           </div>
         )}
+
         <CardHeader className="p-4">
           <CardTitle className="text-xl font-bold text-gray-800">
             {product.name}
           </CardTitle>
-          <CardContent>
-            {product.description && (
-              <p className="text-gray-600 text-sm mb-2">{product.description}</p>
-            )}
-            {price && price.unit_amount && (
-              <p className="text-xl text-white"> ${(price.unit_amount / 100).toFixed(2)}</p>
-            )}
-            <Button className="mt-4 bg-black text-white">View Details</Button>
-          </CardContent>
         </CardHeader>
-      </Card>
-    </Link>
-  )
-}
+
+        <CardContent className="p-4 pt-0">
+          {product.description && (
+            <p className="text-gray-600 text-sm mb-2">{product.description}</p>
+          )}
+          {price?.unit_amount && (
+            <p className="text-xl text-black font-semibold">
+              ${(price.unit_amount / 100).toFixed(2)}
+            </p>
+          )}
+          <Button className="mt-4 w-full bg-black text-white">
+            View Details
+          </Button>
+        </CardContent>
+      </Link>
+    </Card>
+  );
+};
