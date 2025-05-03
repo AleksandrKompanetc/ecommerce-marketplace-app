adf007a4457d5454e33afa02ea67cd0bdfb5015a
AleksandrKompanetc <moneymacker7@gmail.com>
2025-05-03 23:11:34 +0300

    changed Home page

--- app/page.tsx
+++ app/page.tsx
@@ -13,29 +13,28 @@ export default async function Home() {
   })
 
   return (
-    <section className="py-8 px-4 grid grid-cols-1 md:grid-cols-2 gap-8 items-center">
-      <section>
+    <div className="container mx-auto px-4 text-blue-500">
+      <section className="py-12 grid grid-cols-1 md:grid-cols-2 gap-8 items-center">
         <div>
-          <div>
-            <h2>Welcome to My Ecommerce</h2>
-            <p>Discover the latest products at the best prices.</p>
-            <Button asChild variant="default">
-              <Link href="/products">Browse All Products</Link>
-            </Button>
-          </div>
-          <div className="relative w-[450px] h-[450px]">
-            <Image
-              alt="Banner Image"
-              src={products.data[0].images[0]}
-              fill
-              className="object-cover rounded-xl"
-            />
-          </div>
+          <h2 className="text-3xl font-bold mb-4">Welcome to My Ecommerce</h2>
+          <p className="mb-6">Discover the latest products at the best prices.</p>
+          <Button asChild variant="default">
+            <Link href="/products">Browse All Products</Link>
+          </Button>
+        </div>
+        <div className="relative w-[450px] h-[450px]">
+          <Image
+            alt="Banner Image"
+            src={products.data[0].images[0]}
+            fill
+            className="object-cover rounded-xl"
+          />
         </div>
       </section>
-      <section>
+
+      <section className="py-12">
         <Carousel products={products.data} />
       </section>
-    </section>
+    </div>
   );
 }
