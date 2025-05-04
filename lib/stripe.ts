a18a293309a625f1d07ed82350befdaacfc22426
AleksandrKompanetc <moneymacker7@gmail.com>
2025-05-04 23:50:35 +0300

    changed stripe

--- lib/stripe.ts
+++ lib/stripe.ts
@@ -1,3 +1,9 @@
 import Stripe from "stripe";
 
-export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!);
+export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
+  apiVersion: "2023-10-16", // или последняя стабильная
+});
+
+// import Stripe from "stripe";
+
+// export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!);
